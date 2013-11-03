e3ce78e24ee1576dff6fcdb3e12e3c201555d5ea
Justin Russell <jus.russell@gmail.com>
2013-11-03 18:54:49 -0500

    feature: Basic theme done

--- src/scripts/controllers/main.js
+++ src/scripts/controllers/main.js
@@ -1,4 +1,14 @@
 /*global angular */
 angular.module('cm').controller('MainCtrl', ['$scope', function($scope) {
-  $scope.markdown_content = '# Hello World';
+  $scope.markdown_content = [
+    '# Hello World',
+    '- super',
+    '- awesome',
+    '- list',
+    '\n',
+    '# Second page',
+    '```',
+    '// Heyo some code',
+    '```'
+  ].join('\n');
 }]);
