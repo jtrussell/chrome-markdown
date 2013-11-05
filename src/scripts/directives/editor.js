c8c95aa0e2f90bb6d8b5c7a99159c010c1961310
Justin Russell <jus.russell@gmail.com>
2013-11-05 10:59:29 -0500

    style: Changes editor theme and some border colors

--- src/scripts/directives/editor.js
+++ src/scripts/directives/editor.js
@@ -2,10 +2,13 @@
 
 'use strict';
 
+// Look here for available themes:
+// https://github.com/ajaxorg/ace/tree/master/lib/ace/theme
+
 angular.module('cm').directive('editor', [function() {
   return {
     restrict: 'AE',
-    template: '<div ui-ace="{mode: \'markdown\'}"></div>',
+    template: '<div ui-ace="{mode: \'markdown\', theme: \'tomorrow_night\'}"></div>',
     replace: true,
     link: function(scope, element, attrs) {
       // ...
